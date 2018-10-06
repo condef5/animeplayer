08b83bd50514e2df1ff7b0e9db862e81f1d1552f
condef5 <davis.con.fab@gmail.com>
2018-10-06 15:52:39 -0500

    Updates

---
+++ precache-manifest.a287870b30b50fa3b35b99ac8d5042ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6120be815d5d40701515",
+    "url": "/static/js/main.6120be81.chunk.js"
+  },
+  {
+    "revision": "bd64158b65630575f5e9",
+    "url": "/static/js/1.bd64158b.chunk.js"
+  },
+  {
+    "revision": "6120be815d5d40701515",
+    "url": "/static/css/main.c2c31b1c.chunk.css"
+  },
+  {
+    "revision": "fea2c4762e314d948871a6eabf4f0513",
+    "url": "/index.html"
+  }
+];
