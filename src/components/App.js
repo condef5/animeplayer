fcd044e62f58d59a3fc3badfc6752c8db7daf8c3
sasalx <oyundanben@gmail.com>
2018-10-08 05:14:51 +0300

    Floating action button added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React from 'react' 
+import React from 'react'
 import Header from './Header'
 import VideoList from './VideoList'
 import Footer from './Footer'
- 
+import Fab from './Fab'
+
 function App() {
- 
-  return (
-    <div id="app">
-      <Header />
-      <VideoList />
-      <Footer />
-    </div>
-  )
+
+	return (
+		<div id="app">
+			<Header />
+			<VideoList />
+			<Fab />
+			<Footer />
+		</div>
+	)
 }
 export default App;
